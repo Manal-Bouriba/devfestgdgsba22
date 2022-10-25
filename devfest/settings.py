54a9d3365145d081f802c5d2da1e1cd5d2334877
Manal-Bouriba <m.bouriba@esi-sba.dz>
2022-10-25 13:17:09 +0100

    removed secret key

--- devfest/settings.py
+++ devfest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pep1lym540z5oajc9@(ehkaxy#x3(f#-9_g!yud@y*10i-ej0p'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
